dadfa1da87d5a9d491db550b5aa5453e1b519d21
saltyJeff <saltyJeff@users.noreply.github.com>
2019-01-26 01:17:04 -0800

    remove pragma once from ino

--- rocketbuild/generate_dump.py
+++ rocketbuild/generate_dump.py
@@ -1,6 +1,5 @@
 def generate_dump(deps):
-    dump = '#pragma once\n'
-    dump += '#include "package.h"\n'
+    dump = '#include "package.h"\n'
     dump += "#define PRINT(X) Serial.print(X);\n"
     dump += "#define PRINTLN(X) Serial.println(X);\n"
     dump += 'void dump() {\n'
