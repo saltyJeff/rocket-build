27f8670fd1565ad97d22e098b8141fad050d625b
saltyJeff <saltyJeff@users.noreply.github.com>
2019-01-25 19:25:36 -0800

    fixed dump method

--- rocketbuild/generate_dump.py
+++ rocketbuild/generate_dump.py
@@ -1,8 +1,11 @@
 def generate_dump(deps):
     dump = '#pragma once\n'
-    dump += "#define PRINT(x) Serial.print(x);\n"
+    dump += '#include "package.h"\n'
+    dump += "#define PRINT(X) Serial.print(X);\n"
+    dump += "#define PRINTLN(X) Serial.println(X);\n"
     dump += 'void dump() {\n'
     for dep in deps:
-        dump += 'PRINT('+dep.name+')\n'
+        dump += 'PRINT("'+dep.name+':\\t")\n'
+        dump += 'PRINTLN(package.'+dep.name+')\n'
     dump += '}'
     return dump
