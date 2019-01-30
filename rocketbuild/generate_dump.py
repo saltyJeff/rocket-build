8c9ad9bea5aa9274de92c839f75ef2f0cfc44515
saltyJeff <saltyJeff@users.noreply.github.com>
2019-01-29 20:02:51 -0800

    introduce DEBUG_LOG_FIELD macro

--- rocketbuild/generate_dump.py
+++ rocketbuild/generate_dump.py
@@ -1,10 +1,7 @@
 def generate_dump(deps):
     dump = '#include "package.h"\n'
-    dump += "#define PRINT(X) Serial.print(X);\n"
-    dump += "#define PRINTLN(X) Serial.println(X);\n"
     dump += 'void dump() {\n'
     for dep in deps:
-        dump += 'PRINT("'+dep.name+':\\t")\n'
-        dump += 'PRINTLN(package.'+dep.name+')\n'
+        dump += 'DEBUG_LOG_FIELD('+dep.name+')\n'
     dump += '}'
     return dump
